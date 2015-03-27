cd84250fbfbe12d62a4953fcd4a3c21bfaeb4cb5
Peter Corazao <pcorazao@nc2media.com>
2015-03-27 15:39:53 -0500

    added asteroid.

---
+++ SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other) {
+
+		if (other.tag == "Boundary") 
+			return;
+
+		if (other.tag == "Player") 
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation); //create a new game object
+
+		Instantiate (explosion, transform.position, transform.rotation); //create a new game object
+
+//			Debug.Log (other.name);
+			Destroy(other.gameObject);
+			Destroy (gameObject);
+
+	}
+
+}
